844d20973fefa4082c29050ed105230259943583
Phabos <prezatf@gmail.com>
2016-10-29 23:06:37 +0200

    index

--- web/index.php
+++ web/index.php
@@ -1,12 +1,18 @@
 <?php
 
-	//use Symfony\Component\Debug\Debug;
+	$env = getenv('WEBSITE_ENV');
+	$isProd = ( $env == 'prod' ? true : false );
 
 	require_once __DIR__.'/../vendor/autoload.php';
-	//Debug::enable();
+
+	if( ! $isProd )
+		Symfony\Component\Debug\Debug::enable();
 
 	$app = new Silex\Application();
-	require __DIR__.'/../config/prod.php';
+	if( $isProd )
+		require __DIR__.'/../config/prod.php';
+	else
+		require __DIR__.'/../config/dev.php';
 	require __DIR__.'/../src/app.php';
 	require __DIR__.'/../src/routes.php';
 	$app->run();
